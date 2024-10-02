9a4ccacdd1864a9507ed9f7d0ae332b13e5e7e32
上野大和 <festajaune.18637@icloud.com>
2024-10-02 11:02:18 +0900

    add: typeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'sample',
+      password: 'sample',
+      database: 'sample',
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
