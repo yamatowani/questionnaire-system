de9f0687f6e4b95f5d0875606d53ffd219ce78d9
上野大和 <festajaune.18637@icloud.com>
2024-10-02 15:24:15 +0900

    complete graphQL env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { AdminUsersModule } from './admin_users/admin_users.module';
 
@@ -17,7 +18,8 @@ import { AdminUsersModule } from './admin_users/admin_users.module';
       database: 'sample',
       synchronize: false,
     }),
-    GraphQLModule.forRoot({
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       playground: true,
       debug: true,
       autoSchemaFile: join(process.cwd(), 'src/shema.gql'),
