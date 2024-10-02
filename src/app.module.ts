b83aada6af22c0eadea1844a58aacea01a510858
上野大和 <festajaune.18637@icloud.com>
2024-10-02 14:47:48 +0900

    add: graphql module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 @Module({
   imports: [
@@ -13,6 +15,9 @@ import { AppService } from './app.service';
       password: 'sample',
       database: 'sample',
       synchronize: false,
+    }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/shema.gql'),
     }),
   ],
   controllers: [AppController],
