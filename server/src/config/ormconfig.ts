9830c24615ca853957521670bd792487a2bfeb7a
上野大和 <festajaune.18637@icloud.com>
2024-10-03 12:43:20 +0900

    change: complete ormconfig file

--- server/src/config/ormconfig.ts
+++ server/src/config/ormconfig.ts
@@ -7,9 +7,9 @@ const source = new DataSource({
   username: 'sample',
   password: 'sample',
   database: 'sample',
-  entities: [__dirname + '/entities/*.ts'],  
-  migrations: [__dirname + '/migration/*.ts'], 
+  entities: ['src/entities/*.ts'],
+  migrations: ['src/migration/*.ts'],
   synchronize: false,
-});
+});  
 
 export default source;
