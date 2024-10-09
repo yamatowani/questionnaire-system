f8e94fa4126bd0aa54858685ac72c9e8a25aaf8c
上野大和 <festajaune.18637@icloud.com>
2024-10-09 16:48:28 +0900

    delete: unnecessary line

--- client/src/app/question/page.tsx
+++ client/src/app/question/page.tsx
@@ -3,7 +3,6 @@ import NewQuestionForm from "@/components/question/new"
 export default function AdminUserPage() {
   return (
     <div>
-       <h1>アンケート作成画面</h1>
        <NewQuestionForm />
     </div>
   )
