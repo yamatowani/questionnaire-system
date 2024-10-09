7f5231991abbe55cf356e12b39bb78f70089b4cf
上野大和 <festajaune.18637@icloud.com>
2024-10-09 17:52:23 +0900

    fix: lint error

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,8 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: 'http://localhost:3001',
-    credentials: false
-  })
+    credentials: false,
+  });
   await app.listen(3000);
 }
 bootstrap();
