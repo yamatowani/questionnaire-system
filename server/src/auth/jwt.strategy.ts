200daee4a33d363dce146dba7f100dde3b58bf9d
上野大和 <festajaune.18637@icloud.com>
2024-10-10 10:48:26 +0900

    add: jwtAuthGuard

--- server/src/auth/jwt.strategy.ts
+++ server/src/auth/jwt.strategy.ts
@@ -9,7 +9,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secretKey',
+      secretOrKey: 'secretKey', // 環境変数から取得するのが理想
     });
   }
 
