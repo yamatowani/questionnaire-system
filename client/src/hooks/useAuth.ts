747096c7dd95fbf852910dcc3b3ddec783cf376c
上野大和 <festajaune.18637@icloud.com>
2024-10-10 17:09:33 +0900

    update: useAuth

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,33 +1,18 @@
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import jwt, { JwtPayload } from 'jsonwebtoken';
 
 const useAuth = () => {
   const router = useRouter();
-  const [adminUserId, setAdminUserId] = useState<number | null>(null);
 
-  useEffect(() => {
-    const token = localStorage.getItem('token');
-
-    if (!token) {
-      router.push('/login');
-    } else {
-      try {
-        const decodedToken = jwt.decode(token) as JwtPayload;
-        setAdminUserId(Number(decodedToken.sub));
-      } catch (error) {
-        console.error('Token decoding error:', error);
-        router.push('/login');
-      }
-    }
-  }, [router]);
+  const getToken = () => {
+    return localStorage.getItem('token');
+  };
 
   const logout = () => {
     localStorage.removeItem('token');
     router.push('/login');
   };
 
-  return { logout, adminUserId };
+  return { logout, getToken };
 };
 
 export default useAuth;
