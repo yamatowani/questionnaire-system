c5bb007c182a348f9286698ecdf2bb1b9f2d8a8d
上野大和 <festajaune.18637@icloud.com>
2024-10-10 17:21:02 +0900

    change: redirect /login when token is null

--- client/src/hooks/useAuth.ts
+++ client/src/hooks/useAuth.ts
@@ -1,9 +1,17 @@
+import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
 const useAuth = () => {
   const router = useRouter();
 
   const token = localStorage.getItem('token');
+
+  useEffect(() => {
+
+    if (!token) {
+      router.push('/login'); 
+    }
+  }, [router, token]);
 
   const logout = () => {
     localStorage.removeItem('token');
