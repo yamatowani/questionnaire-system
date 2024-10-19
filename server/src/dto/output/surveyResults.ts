f941a0efafccc8db3a6deb5609e432985d880e0e
上野大和 <festajaune.18637@icloud.com>
2024-10-19 18:25:30 +0900

    add: surveyResults DTO

---
+++ server/src/dto/output/surveyResults.ts
@@ -0,0 +1,37 @@
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class SurveyResults {
+  @Field(() => Int)
+  surveyId: number;
+
+  @Field()
+  title: string;
+
+  @Field(() => [Questions])
+  questions: Questions[];
+}
+
+@ObjectType()
+class Questions {
+  @Field(() => Int)
+  questionId: number;
+
+  @Field()
+  questionText: string;
+
+  @Field(() => [AnswerCounts])
+  questionResults: AnswerCounts[];
+}
+
+@ObjectType()
+class AnswerCounts {
+  @Field(() => Int)
+  option_id: number;
+
+  @Field()
+  optionText: string;
+
+  @Field(() => Int)
+  count: number;
+}
