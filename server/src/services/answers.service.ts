87678ea2fa466f3b05b7493b36cc69e4dd3b49f3
上野大和 <festajaune.18637@icloud.com>
2024-10-22 20:05:09 +0900

    update: validation logic

--- server/src/services/answers.service.ts
+++ server/src/services/answers.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { EntityManager, Repository } from 'typeorm';
+import { EntityManager, In, Repository } from 'typeorm';
 import { Question } from 'src/entities/question.entity';
 import { Option } from 'src/entities/option.entity';
 import { Answer } from 'src/entities/answer.entity';
@@ -11,14 +11,64 @@ export class AnswerService {
   constructor(
     @InjectRepository(Answer)
     private readonly answerRepository: Repository<Answer>,
-    @InjectRepository(Question)
-    private readonly questionRepository: Repository<Question>,
-    @InjectRepository(Option)
-    private readonly optionRepository: Repository<Option>,
   ) {}
 
   public async create(submitAnswerInput: SubmitAnswerInput): Promise<Answer[]> {
     const answers: Answer[] = [];
+
+    for (const questionAnswer of submitAnswerInput.question_answers) {
+      const { question_id, options } = questionAnswer;
+
+      const relatedQuestion = await this.answerRepository.manager.findOne(
+        Question,
+        {
+          where: { id: question_id },
+        },
+      );
+
+      if (options.length === 0) {
+        throw new BadRequestException(
+          `質問 "${relatedQuestion.question_text}" に回答してください`,
+        );
+      }
+
+      const relatedOptions = await this.answerRepository.manager.find(Option, {
+        where: { id: In(options.map((option) => option.option_id)) },
+      });
+
+      let otherSelected = false;
+
+      for (const selectedOption of options) {
+        const { option_id, other_response } = selectedOption;
+        const relatedOption = relatedOptions.find(
+          (option) => option.id === option_id,
+        );
+
+        if (!relatedOption) {
+          throw new BadRequestException(`無効な選択肢が含まれています。`);
+        }
+
+        if (relatedOption.option_text === 'その他') {
+          if (!other_response) {
+            throw new BadRequestException(
+              `質問 "${relatedQuestion.question_text}" で「その他」を選択した場合はテキストを入力してください`,
+            );
+          }
+          otherSelected = true;
+        }
+      }
+
+      if (!otherSelected) {
+        for (const selectedOption of options) {
+          const { other_response } = selectedOption;
+          if (other_response) {
+            throw new BadRequestException(
+              `質問 "${relatedQuestion.question_text}" で「その他」以外を選択した場合はテキストは入力しないでください`,
+            );
+          }
+        }
+      }
+    }
 
     return await this.answerRepository.manager.transaction(
       async (entityManager: EntityManager) => {
@@ -28,30 +78,17 @@ export class AnswerService {
           const relatedQuestion = await entityManager.findOne(Question, {
             where: { id: question_id },
           });
-          if (options.length === 0) {
-            throw new BadRequestException(
-              `質問 "${relatedQuestion.question_text}" に回答してください`,
-            );
-          }
+
+          const relatedOptions = await entityManager.find(Option, {
+            where: { id: In(options.map((option) => option.option_id)) },
+          });
 
           for (const selectedOption of options) {
             const { option_id, other_response } = selectedOption;
 
-            const relatedOption = await entityManager.findOne(Option, {
-              where: { id: option_id },
-            });
-
-            if (relatedOption.option_text === 'その他' && !other_response) {
-              throw new BadRequestException(
-                `質問 "${relatedQuestion.question_text}" で「その他」を選択した場合はテキストを入力してください`,
-              );
-            }
-
-            if (relatedOption.option_text !== 'その他' && other_response) {
-              throw new BadRequestException(
-                `質問 "${relatedQuestion.question_text}" で「その他」以外を選択した場合はテキストは入力しないでください`,
-              );
-            }
+            const relatedOption = relatedOptions.find(
+              (option) => option.id === option_id,
+            );
 
             const createdAnswer = entityManager.create(Answer, {
               question: relatedQuestion,
